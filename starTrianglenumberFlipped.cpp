3460704cc0c6e9fd302a3b75900eb45825cc6a8a
Swagat2 <Shreemanswagat2002@gmail.com>
2025-07-27 13:26:04 +0530

    Added new

---
+++ starTrianglenumberFlipped.cpp
@@ -0,0 +1,21 @@
+// 4
+// 4 3
+// 4 3 2
+// 4 3 2 1
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4; // You can take input too: cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 4; j >= 5 - i; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
