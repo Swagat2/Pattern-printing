4b69eb13d14de832582dbd84a600bda161959290
Swagat2 <Shreemanswagat2002@gmail.com>
2025-07-29 18:27:51 +0530

    New questions

--- starCross.cpp
+++ starCross.cpp
@@ -1,4 +1,28 @@
-* * * * * * 
-*         *
-*         *
-* * * * * *
+// *       *
+//   *   *
+//     *
+//   *   *
+// *       *
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Number of side rows  :";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j == i || j == n - i + 1)
+            {
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    } 
+}
