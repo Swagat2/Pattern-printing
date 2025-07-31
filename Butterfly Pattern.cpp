77f61b4a4f50710bbd15a3aeac6363b9466d8b4a
Swagat2 <Shreemanswagat2002@gmail.com>
2025-07-31 19:48:09 +0530

    added new

--- Butterfly Pattern.cpp
+++ Butterfly Pattern.cpp
@@ -22,5 +22,10 @@ int main() {
         cout << endl;
     }
     // Lower Half
-
+    for(int i = n; i >= 1; i--) {
+        for(int j = 1; j <= i; j++) cout << "*";
+        for(int j = 1; j <= 2*(n - i); j++) cout << " ";
+        for(int j = 1; j <= i; j++) cout << "*";
+        cout << endl;
+    }
 }
