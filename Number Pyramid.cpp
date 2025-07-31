533eb6561243235932f7c4e3acb95dfad4a84950
Swagat2 <Shreemanswagat2002@gmail.com>
2025-07-31 19:50:39 +0530

    added new

---
+++ Number Pyramid.cpp
@@ -0,0 +1,20 @@
+//        1 
+//       1 2 1
+//     1 2 3 2 1
+//   1 2 3 4 3 2 1
+// 1 2 3 4 5 4 3 2 1
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for(int i = 1; i <= n; i++) {
+        // Spaces
+        for(int j = 1; j <= n - i; j++) cout << "  ";
+        // Increasing Numbers
+        for(int j = 1; j <= i; j++) cout << j << " ";
+        // Decreasing Numbers
+        for(int j = i - 1; j >= 1; j--) cout << j << " ";
+        cout << endl;
+    }
+}
